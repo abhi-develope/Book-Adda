35e9d2d95589beb5fd14a677b996da51bd43ba39
abhi-develope <prajapatiabhishek320@gmail.com>
2024-06-09 23:44:56 +0530

    sticy navbar on scroll

--- frontend/src/componets/Navbar.jsx
+++ frontend/src/componets/Navbar.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Navbar() {
+    const [sticky, setStiky] = useState(false)
+    useEffect(()=>{
+       const handleScroll=()=>{
+        if(window.scrollY > 0){
+            setStiky(true)
+        }
+        else{
+            setStiky(false)
+        }
+       } 
+       window.addEventListener('scroll',handleScroll)
+       return ()=>{
+        window.removeEventListener('scroll',handleScroll)
+       }
+    },[])
   const navItem = (
     <>
       <li>
@@ -18,8 +33,8 @@ function Navbar() {
     </>
   );
   return (
-    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0">
-      <div className="navbar bg-base-100">
+    <div className={`max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 ${sticky?"sticky-navbar shadow-lg bg-base-200 duration-300 transition-all ease-in-out" : ""}`}>
+      <div className="navbar ">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
