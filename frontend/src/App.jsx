871668a2ed9d1adc65622a6433b6215117f51c73
abhi-develope <prajapatiabhishek320@gmail.com>
2024-06-15 17:45:42 +0530

    add crousel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './componets/Navbar'
 import Banner from './componets/Banner'
 import Footer from './componets/Footer'
+import Freebook from './componets/Freebook'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Banner/>
+      <Freebook/>
       <Footer/>
     </div>
   )
--- frontend/src/componets/Freebook.jsx
+++ frontend/src/componets/Freebook.jsx
@@ -1,10 +1,88 @@
 import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import list from "../../public/list.json"
 
 function Freebook() {
+
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+
+
+
+   const filterData = list.filter((data) => data.category === "free") 
   return (
+    <>
+    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+     <h1 className='font-semibold text-xl-pb-1'>Free Offered Courses</h1> 
+     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab cupiditate iste fugit reprehenderit sequi? Voluptates cumque dignissimos, sint, saepe maiores minus qui ducimus itaque accusamus nam nobis quae accusantium quas!</p>
+    
     <div>
-      
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+        <div>
+          <h3>7</h3>
+        </div>
+        <div>
+          <h3>8</h3>
+        </div>
+      </Slider>
     </div>
+    </div>
+    </div>
+    
+    </>
   )
 }
 
