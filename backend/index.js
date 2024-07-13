27aa8b3ff75b11a410a73ae322fe4cd2e5e5fe91
abhi-develope <prajapatiabhishek320@gmail.com>
2024-07-13 23:42:33 +0530

    connected to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
 
 dotenv.config();
@@ -8,9 +9,13 @@ const mongoURI = process.env.MOngoDBURI;
 
 //connect to mongodb
 try {
-  
+  mongoose.connect(mongoURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+  });
+  console.log("Connected to mongodb");
 } catch (error) {
-  
+  console.log("Error", error);
 }
 
 app.listen(PORT, () => {
