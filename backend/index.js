e5cc5c67eb4b8a5e91a23508605ee7b89d3254ee
abhi-develope <prajapatiabhishek320@gmail.com>
2024-07-14 09:19:40 +0530

    defining routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+
+import bookRoute from "./route/book.route.js";
+
 const app = express();
 
 dotenv.config();
@@ -9,14 +12,17 @@ const mongoURI = process.env.MOngoDBURI;
 
 //connect to mongodb
 try {
-  mongoose.connect(mongoURI,{
+  mongoose.connect(mongoURI, {
     useNewUrlParser: true,
-    useUnifiedTopology:true
+    useUnifiedTopology: true,
   });
   console.log("Connected to mongodb");
 } catch (error) {
   console.log("Error", error);
 }
+
+// defining routes
+app.use("/book", bookRoute);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
