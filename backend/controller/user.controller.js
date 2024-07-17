4f0f6551e94d27c90cda9c95bd835c0cf085fd03
abhi-develope <prajapatiabhishek320@gmail.com>
2024-07-17 08:22:30 +0530

    write logic in user controller

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,1 +1,25 @@
 import User from "../model/user.model.js";
+
+export const signup = (req, res) => {
+    try {
+        const {fullname, email, password,sex} = req.body;
+        const user = User.findOne({email});
+        if(user) {
+            return res.status(400).json({ message: "user already exists"});
+        }
+
+        const createUser = new User({
+            fullname,
+            email,
+            password,
+            sex
+        });
+        createUser.save();
+        res.status(201).json({message: "user created successfully"});
+    } catch (error) {
+        console.log("Error" + error.message);
+        res.status(500).json({message: "Internal server error"})
+        
+        
+    }
+}
